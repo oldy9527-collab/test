a94ec21086aa6e1299fc7e584ba17f87d0909b33
oldy9527-collab <oldy9527@gmail.com>
2026-01-25 09:18:54 +0800

    Create fire.sh
    32

---
+++ fire.sh
@@ -0,0 +1,12 @@
+sudo ufw allow 22/tpc
+sudo ufw allow 38124/udp
+sudo ufw allow 41287/udp
+sudo ufw allow 45863/udp
+sudo ufw allow 49211/udp
+sudo ufw allow 53789/udp
+
+sudo ufw allow 32241/udp
+sudo ufw allow 35678/udp
+sudo ufw allow 40193/udp
+sudo ufw allow 46652/udp
+sudo ufw allow 52984/udp
