14834dc9482b2334eb61ebc731a3b7b5c530461b
oldy9527-collab <oldy9527@gmail.com>
2026-01-25 09:49:36 +0800

    Update fire.sh
    t

--- fire.sh
+++ fire.sh
@@ -27,10 +27,10 @@ sudo ufw allow 46652/udp
 sudo ufw allow 52984/udp
 
 # 6. 放行 Vless-reality（TCP）
-sudo ufw allow 2443/tcp
+sudo ufw allow 32958/tcp
 
 # 7. 放行 Vmess-ws（TCP）
-sudo ufw allow 2443/tcp
+sudo ufw allow 2088/tcp
 
 # 8. 查看当前防火墙状态
 echo "当前防火墙状态："
